ba06fbcb65ef4f7874ad7c3b493f48f92edd776f
Eric <eric.bichara@gmail.com>
2022-04-12 23:05:08 +0200

    updated models

--- models/user.js
+++ models/user.js
@@ -30,6 +30,10 @@ const userSchema = new mongoose.Schema({
     gender: {
         type: Number,
         required: true
+    },
+    role: {
+        type: Number,
+        required: true
     }
 
 }, {
